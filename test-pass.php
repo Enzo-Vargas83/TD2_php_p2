964f27cad11d61d7be9624718bbb2a42038e7286
Enzo-Vargas83 <enzo.vargas@etu.univ-amu.fr>
2021-12-09 21:55:08 +0100

    rrezrzer

--- test-pass.php
+++ test-pass.php
@@ -1,16 +1,11 @@
 <?php
 if (!empty($_POST)) {
-    session_start();
     @$login=$_POST["login"];
      $dbh = new PDO('mysql:host=mysql-vargas.alwaysdata.net;dbname=vargas_td2', 'vargas', 'lolo83520');
-     $sel=$dbh->prepare("select * from utilisateurs where Login=? limit");
+     $sel=$dbh->prepare("select * from utilisateurs where Login=? limit 1");
      $sel->execute(array($login));
      $tab=$sel->fetchAll();
      if(count($tab)>0){
-         $_SESSION["prenomNom"]=ucfirst(strtolower($tab[0]["prenom"])).
-                " ".strtoupper($tab[0]["nom"]);
-         $_SESSION["autoriser"]="oui";
-            header("location:session.php");
          echo "connecter";
         }
         else {
