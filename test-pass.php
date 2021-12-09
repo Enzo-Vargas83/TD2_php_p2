a9b2a680ae9d94b431168761fac30162fcc571ce
Enzo-Vargas83 <enzo.vargas@etu.univ-amu.fr>
2021-12-09 21:22:05 +0100

    rrezrzer

--- test-pass.php
+++ test-pass.php
@@ -4,10 +4,9 @@ if (!empty($_POST)) {
     $dbh = new PDO('mysql:host=mysql-vargas.alwaysdata.net;dbname=vargas_td2', 'vargas', 'lolo83520');
     $stmt = $dbh->prepare("SELECT *  FROM User_test WHERE Login =  :Login");
     //On recupère les pseudo de t'as base ou les pseudo son egal au pseudo passer par le formulaire
-    $stmt->bindValue('pseudo', $login, PDO::PARAM_STR);
+    $stmt->bindValue('pseudo', $login);
     $stmt->execute();
     //on exécute la requête
-
     $pseudoINbdd = $stmt->fetchColumn();
     if($pseudoINbdd !== 0)
      {
