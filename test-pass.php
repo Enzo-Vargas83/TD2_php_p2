098d92b1eaa8adbe8b5f1c2834127115ea39948a
Enzo-Vargas83 <enzo.vargas@etu.univ-amu.fr>
2021-12-09 17:05:21 +0100

    oui 4

--- test-pass.php
+++ test-pass.php
@@ -7,7 +7,7 @@ if (!empty($_POST)) {
     $select = mysqli_query($dbLink, "SELECT COUNT(Login) FROM User_test WHERE Login = '" . $_POST['login'] . "'");
     var_dump($select);
     $nombre = mysqli_fetch_array($select);
-    if ($nombre == 1) {
+    if ($nombre[0] == 1) {
         header("location : page_erreur.php");
     } else {
         header("location : page_bienvenue.php");
