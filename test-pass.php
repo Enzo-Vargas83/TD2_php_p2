3007c63e0826a13207575fae428239e92b4b72d8
Enzo-Vargas83 <enzo.vargas@etu.univ-amu.fr>
2021-12-09 21:34:47 +0100

    rrezrzer

--- test-pass.php
+++ test-pass.php
@@ -2,7 +2,7 @@
 if (!empty($_POST)) {
     $login = $_POST['login'];
     $dbh = new PDO('mysql:host=mysql-vargas.alwaysdata.net;dbname=vargas_td2', 'vargas', 'lolo83520');
-    $stmt = $dbh->prepare("SELECT * FROM User_test WHERE username=?");
+    $stmt = $dbh->prepare("SELECT * FROM User_test WHERE Login=?");
     $stmt->execute([$login]);
     $user = $stmt->fetch();
     var_dump($user);
