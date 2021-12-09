130f7ffd5352f437a4b57eba5e15fe7f0e8f426e
Enzo-Vargas83 <enzo.vargas@etu.univ-amu.fr>
2021-12-09 16:38:27 +0100

    oui 4

--- test-pass.php
+++ test-pass.php
@@ -5,7 +5,7 @@ if (!empty($_POST)) {
     $dbLink = mysqli_connect('mysql-vargas.alwaysdata.net', 'vargas', 'lolo83520', 'vargas_td2');
 
     $select = mysqli_query($dbLink, "SELECT Login FROM User_test WHERE Login = '". $_POST['login'] ."'");
-    echo var_dump($select);
+    var_dump($select);
     if(mysqli_num_rows($select)) {
         header("location : page_erreur.php");
     }
