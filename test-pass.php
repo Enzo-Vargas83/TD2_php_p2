48a4f6ee372ab572248db3698f6bba7a22439450
Enzo-Vargas83 <enzo.vargas@etu.univ-amu.fr>
2021-12-09 20:39:08 +0100

    g('zegez'vrz

--- test-pass.php
+++ test-pass.php
@@ -1,7 +1,7 @@
 <?php
 if (!empty($_POST)) {
     $dbh = new PDO('mysql:host=mysql-vargas.alwaysdata.net;dbname=vargas_td2', 'vargas', 'lolo83520');
-    $stmt = $dbh->prepare("SELECT COUNT(Login) FROM User_test WHERE Login = ?");
+    $stmt = $dbh->prepare("SELECT *  FROM User_test WHERE Login = ?");
     $stmt->execute([
         $_POST['login']
     ]);
