d8ac13356613fa689e539400f420841f776f5cd9
Enzo-Vargas83 <enzo.vargas@etu.univ-amu.fr>
2021-12-09 16:31:54 +0100

    oui 4

--- test-pass.php
+++ test-pass.php
@@ -3,13 +3,5 @@ if (!empty($_POST)) {
     $login = $_POST['login'];
     $mdp = $_POST['password'];
     $dbLink = mysqli_connect('mysql-vargas.alwaysdata.net', 'vargas', 'lolo83520', 'vargas_td2');
-
-    $select = mysqli_query($dbLink, "SELECT Login FROM User_test WHERE Login = '". $_POST['login'] ."'");
-    if(mysqli_num_rows($select)) {
-        header("location : page_erreur.php");
-    }
-    else {
-        header("location : page_bienvenue.php");
-    }
 }
 ?>
