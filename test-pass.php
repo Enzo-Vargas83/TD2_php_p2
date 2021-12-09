e594547c0df2e737dbe358539045eb2658efe261
Enzo-Vargas83 <enzo.vargas@etu.univ-amu.fr>
2021-12-09 21:11:32 +0100

    rrezrzer

--- test-pass.php
+++ test-pass.php
@@ -2,16 +2,11 @@
 if (!empty($_POST)) {
     $dbh = new PDO('mysql:host=mysql-vargas.alwaysdata.net;dbname=vargas_td2', 'vargas', 'lolo83520');
     $stmt = $dbh->prepare("SELECT *  FROM User_test WHERE Login = ?");
-    $stmt->execute(array(
-        $_POST['login']));
+    $stmt->execute([
+        $_POST['login']
+    ]);
 
-    $resultat = $stmt->fetch();
-
-    if (!$resultat)
-    {
-        if (!$resultat['login'] === $username) {
-            array_push($errors, "Ce nom d'utilisateur existe déjà");
-        }
-    }
+    $result = $stmt->fetchAll();
+    var_dump($result);
 }
 ?>
