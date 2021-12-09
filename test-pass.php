94d01827490ea9860280264a48a6de251181b2c0
Enzo-Vargas83 <enzo.vargas@etu.univ-amu.fr>
2021-12-09 20:40:46 +0100

    g('zegez'vrz

--- test-pass.php
+++ test-pass.php
@@ -2,6 +2,8 @@
 if (!empty($_POST)) {
     var_dump($_POST['login']);
 
+    phpinfo();
+
     $dbh = new PDO('mysql:host=mysql-vargas.alwaysdata.net;dbname=vargas_td2', 'vargas', 'lolo83520');
     $stmt = $dbh->prepare("SELECT *  FROM User_test WHERE Login = ?");
     $stmt->execute([
