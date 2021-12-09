9715d595dd7147c9ae1691d58a652e33d5a20035
Enzo-Vargas83 <enzo.vargas@etu.univ-amu.fr>
2021-12-09 22:20:54 +0100

    rrezrzer

--- test-pass.php
+++ test-pass.php
@@ -3,14 +3,8 @@ if (!empty($_POST)) {
     $login=$_POST["login"];
      $dbh = new PDO('mysql:host=mysql-vargas.alwaysdata.net;dbname=vargas_td2', 'vargas', 'lolo83520');
      $stmt=$dbh->prepare("select * from User_test where Login=?");
-     $stmt->execute([$login]);
      $stmt->fetch();
+     $stmt->execute([$login]);
      var_dump($stmt);
-     if(\mysql_xdevapi\Statement::$stmt == 1) {
-        echo "existe";
-     }
-     else {
-        echo 'aucun utilisateur ayant id_user égale à 2';
-     }
 }
 ?>
