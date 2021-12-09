357c45a02b2ff97f18184b7010e2ae5d5756bfbd
Enzo-Vargas83 <enzo.vargas@etu.univ-amu.fr>
2021-12-09 17:23:32 +0100

    oui 4

--- test-pass.php
+++ test-pass.php
@@ -5,7 +5,6 @@ if (!empty($_POST)) {
     $dbLink = mysqli_connect('mysql-vargas.alwaysdata.net', 'vargas', 'lolo83520', 'vargas_td2');
 
     $select = mysqli_query($dbLink, "SELECT COUNT(Login) FROM User_test WHERE Login = '" . $_POST['login'] . "'");
-    $nombre = mysqli_fetch_row($select);
-    echo $nombre[0];
+    var_dump($select);
     }
 ?>
