fcc7a7461484766241a56dadbd9f5e6e148e5b78
Enzo-Vargas83 <enzo.vargas@etu.univ-amu.fr>
2021-12-09 22:09:25 +0100

    rrezrzer

--- test-pass.php
+++ test-pass.php
@@ -2,15 +2,13 @@
 if (!empty($_POST)) {
     @$login=$_POST["login"];
      $dbh = new PDO('mysql:host=mysql-vargas.alwaysdata.net;dbname=vargas_td2', 'vargas', 'lolo83520');
-     $sel=$dbh->prepare("select * from utilisateurs where Login=? limit 1");
-     $sel->execute(array($login));
-     $tab=$sel->fetchAll();
-     if(count($tab)>0){
-         echo "connecter";
-        }
-        else {
-            $erreur = "Mauvais login ou mot de passe!";
-            echo $erreur;
-        }
+     $stmt=$dbh->prepare("select * from utilisateurs where Login=?");
+     $stmt->execute([$login]);
+     if(FALSE !== $stmt->fetch()) {
+        echo "existe";
+     }
+     else {
+        echo 'aucun utilisateur ayant id_user égale à 2';
+     }
 }
 ?>
