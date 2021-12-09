cb360d7b2c00b347d6e46d8c5237e678e0f632be
Enzo-Vargas83 <enzo.vargas@etu.univ-amu.fr>
2021-12-09 16:55:56 +0100

    oui 4

--- test-pass.php
+++ test-pass.php
@@ -4,12 +4,9 @@ if (!empty($_POST)) {
     $mdp = $_POST['password'];
     $dbLink = mysqli_connect('mysql-vargas.alwaysdata.net', 'vargas', 'lolo83520', 'vargas_td2');
 
-    $select = $dbLink->prepare("SELECT Login FROM User_test WHERE Login = '" . $_POST['login'] . "'");
+    $select = mysqli_query($dbLink, "SELECT Login FROM User_test WHERE Login = '" . $_POST['login'] . "'");
     var_dump($select);
-    $select->execute(array($_POST['numero']));
-    $number = $select->fetch();
-
-    if ($number['numerp'] == 1) {
+    if ($select == 1) {
         header("location : page_erreur.php");
     } else {
         header("location : page_bienvenue.php");
