dba8960a294dd71b6c474026c9cf203002ed8397
Enzo-Vargas83 <enzo.vargas@etu.univ-amu.fr>
2021-12-09 16:48:35 +0100

    oui 4

--- test-pass.php
+++ test-pass.php
@@ -7,5 +7,11 @@ if (!empty($_POST)) {
     $select = mysqli_query($dbLink, "SELECT Login FROM User_test WHERE Login = '". $_POST['login'] ."'");
     var_dump($select);
 
+    if($select->num_rows == 0) {
+        header("location : page_erreur.php");
+    }
+    else {
+        header("location : page_bienvenue.php");
+    }
 }
 ?>
